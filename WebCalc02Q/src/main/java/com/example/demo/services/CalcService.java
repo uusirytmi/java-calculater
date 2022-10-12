480eed2d54dfaa1938dfd29f62c3ac976fbfa26c
uusirytmi <72684376+uusirytmi@users.noreply.github.com>
2022-10-12 13:27:41 +0900

    Update CalcService.java
    四則計算において数字以外の文字列が入力されたときにエラーが発生するよう変更
    無限小数が発生する割り算をエラーではなく小数7桁まで表示するように変更

--- WebCalc02Q/src/main/java/com/example/demo/services/CalcService.java
+++ WebCalc02Q/src/main/java/com/example/demo/services/CalcService.java
@@ -1,6 +1,7 @@
 package com.example.demo.services;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,16 @@ public class CalcService {
 			String NumB
 
 	) {
-		BigDecimal AdA = new BigDecimal(NumA);
-		BigDecimal AdB = new BigDecimal(NumB);
+		BigDecimal AdA = null;
+		BigDecimal AdB = null;
+		try {
+			AdA = new BigDecimal(NumA);
+			AdB = new BigDecimal(NumB);
+		} catch (Exception e) {
+			return "数字以外の文字列が入っています。もう一度入力してください。";
+			// TODO 自動生成された catch ブロック
+
+		}
 		BigDecimal AdR = AdA.add(AdB);
 		String AdResult = AdR.toString();
 		return AdResult;
@@ -27,8 +36,15 @@ public class CalcService {
 
 	{
 
-		BigDecimal SbA = new BigDecimal(NumA);
-		BigDecimal SbB = new BigDecimal(NumB);
+		BigDecimal SbA = null;
+		BigDecimal SbB = null;
+		try {
+			SbA = new BigDecimal(NumA);
+			SbB = new BigDecimal(NumB);
+		} catch (Exception e) {
+			return "数字以外の文字列が入っています。もう一度入力してください。";
+			// TODO 自動生成された catch ブロック
+		}
 		BigDecimal SbR = SbA.subtract(SbB);
 		String SbResult = SbR.toString();
 		return SbResult;
@@ -41,8 +57,16 @@ public class CalcService {
 			String NumB
 
 	) {
-		BigDecimal MlA = new BigDecimal(NumA);
-		BigDecimal MlB = new BigDecimal(NumB);
+		BigDecimal MlA = null;
+		BigDecimal MlB = null;
+		try {
+			MlA = new BigDecimal(NumA);
+			MlB = new BigDecimal(NumB);
+		} catch (Exception e) {
+			return "数字以外の文字列が入っています。もう一度入力してください。";
+
+			// TODO 自動生成された catch ブロック
+		}
 		BigDecimal MlR = MlA.multiply(MlB);
 		String MlResult = MlR.toString();
 		return MlResult;
@@ -55,17 +79,19 @@ public class CalcService {
 			String NumB
 
 	) {
-		BigDecimal DvA = new BigDecimal(NumA);
-		BigDecimal DvB = new BigDecimal(NumB);
-
+		BigDecimal DvA = null;
+		BigDecimal DvB = null;
 		try {
-			BigDecimal DvR = DvA.divide(DvB);
-			String DvResult = DvR.toString();
-			return DvResult;
+			DvA = new BigDecimal(NumA);
+			DvB = new BigDecimal(NumB);
 		} catch (Exception e) {
-			return "無限小数の答えになりました";
+			return "数字以外の文字列が入っています。もう一度入力してください。";
 
+			// TODO 自動生成された catch ブロック
 		}
+		BigDecimal DvR = DvA.divide(DvB, 7, RoundingMode.HALF_UP);
+		String DvResult = DvR.toString();
+		return DvResult;
 
 		//		割り算に関しては特殊なので注意
 		//		
